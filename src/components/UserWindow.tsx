7b5416d05c96e45f10333d810f2d3493c56842cf
JamPanda-dev <106949016+JamPanda-dev@users.noreply.github.com>
2022-06-27 13:50:05 +0900

    Edit src/components/UserWindow.js

--- src/components/UserWindow.tsx
+++ src/components/UserWindow.tsx
@@ -1,13 +1,24 @@
 import { useWindowDimensions } from '../hook/getWindowDimensions'
 import React, {useEffect, useState} from 'react'
 import './UserWindow.css'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+library.add(faDownload)
 export const UserWindow:React.FC = () => {
   const [windowWidth, setWindowWidth] = useState<number>()  
   const [windowHeight, setWindowHeight] = useState<number>()
   const {width, height} = useWindowDimensions()
   return (
     <>
-     <span className='head'></span>
+     <span className='head'>
+        <span className='btn_'>
+            <FontAwesomeIcon icon={faDownload} className='fa__i_'/> DOWNLOAD
+        </span>
+        <span className='controllholder'>
+            
+        </span>
+     </span>
     </>
   )
 }
