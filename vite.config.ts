e084954015d837139e4316227ec3acfea9b91121
JamPanda330 <106949016+JamPanda-dev@users.noreply.github.com>
2022-07-03 08:28:11 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.GITHUB_VA_ ? "/3dtext-generator/" ? "/"
+  })
+}    
