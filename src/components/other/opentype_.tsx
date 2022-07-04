279cdc444172ecd66d335ab8a440786108a18405
JamPanda330 <106949016+JamPanda-dev@users.noreply.github.com>
2022-07-04 11:12:14 +0900

    :bug: Fix Github Actions Bug

--- src/components/other/opentype_.tsx
+++ src/components/other/opentype_.tsx
@@ -1,20 +1,20 @@
 import opentype from 'opentype.js';
 import {useState} from 'react'
 //const [mysvg, setmysvg] = useState<any>()
-const optimizationText = (text, width, height, fontsize_, base_) => {
+const optimizationText = (text: any, width: any, height: any, fontsize_: any, base_: any) => {
 //  var _svg = ''
-    async function makeText(input, x, y, fontsize, _base64) {
+    async function makeText(input: any, x: any, y: any, fontsize: any, _base64: any) {
       const font = await opentype.load(_base64);
       const path = font.getPath(input, 0, 150, 72).toPathData();
       return path
     }
-   const len_ = (txt) => {
+   const len_ = (txt: any) => {
      if (txt !== undefined) {
        return txt.length
      }
      return 0
    }
-   function get_v(value) {
+   function get_v(value: any) {
      return value;
    }
    const svg_path = makeText(text, (width -  len_(text) * fontsize_) / 2, height / 2, fontsize_, base_)
